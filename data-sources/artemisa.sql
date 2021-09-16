e0faa34ad4e498cee2716d22b1ff6e4807e92476
Roberto Carlos Trujillo Torres <RobertoCarlos.TrujilloTorres@infobip.com>
2021-09-16 12:30:29 -0500

    Add basic definition for DB init sql

---
+++ data-sources/artemisa.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+)
